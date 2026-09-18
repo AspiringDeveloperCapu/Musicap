f3f38f8ef6ccd0827117a9a4e5a14868c2ece15c
Aaron Jacob Capulong <jacobcapulong123@gmail.com>
2026-09-18 08:05:59 +0800

    Redesign dashboard with search in AppBar, recently played, recommendations, playlists

--- lib/main.dart
+++ lib/main.dart
@@ -45,16 +45,17 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
 
-  final List<Widget> _pages = [
-    const HomePage(),
-    const PlaylistPage(),
-    const SettingsPage(),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [
+          HomePage(),
+          PlaylistPage(),
+          SettingsPage(),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
